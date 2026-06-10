0b8954abd4808416032bb54cca2c28c001c5ac91
Satya-511 <250320100053@cutm.ac.in>
2026-06-10 11:57:11 +0530

    jdbc

---
+++ src/main/java/Book_Management_System/Book_Details.java
@@ -0,0 +1,31 @@
+package Book_Management_System;
+
+public class Book_Details {
+    private int Id;
+    private String Name;
+    private  int Price;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+}
---
+++ src/main/java/Book_Management_System/ConnectDb.java
@@ -0,0 +1,82 @@
+package Book_Management_System;
+
+import java.sql.*;
+
+public class ConnectDb {
+
+    private final String url = "jdbc:mysql://localhost:3306/database_ichigo";
+    private final String user = "root";
+    private final String password = "Nayak@1234";
+    private static Connection connection=null;
+    public  void connect(){
+        try {
+            connection= DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public int insert(Book_Details b){
+        String sql="insert into books values(?,?,?)";
+        try {
+            PreparedStatement prt =connection.prepareStatement(sql);
+            prt.setInt(1,b.getId());
+            prt.setString(2, b.getName());
+            prt.setInt(3,b.getPrice());
+            return prt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public boolean delete(int id){
+        String sql="delete from books where Id=?";
+        try {
+            PreparedStatement prt=connection.prepareStatement(sql);
+            prt.setInt(1,id);
+            if(prt.executeUpdate()>0){
+                return  true;
+            }
+            return  false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public int update(Book_Details b){
+        String sql="update books set Name=?,Price=? where Id=?";
+        try {
+            PreparedStatement prt=connection.prepareStatement(sql);
+            prt.setString(1,b.getName());
+            prt.setInt(2,b.getPrice());
+            prt.setInt(3,b.getId());
+            return prt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void Search(int id){
+        String sql="select*from books where Id=?";
+        try {
+            PreparedStatement prt=connection.prepareStatement(sql);
+            prt.setInt(1,id);
+            ResultSet rs= prt.executeQuery();
+            if(rs.next()){
+                System.out.println(rs.getInt(1)+"|"+rs.getString(2)+"|"+rs.getInt(3));
+            }else
+                System.out.println("Book not found");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public  void display(){
+        String sql="select * from books";
+        try {
+            Statement smt= connection.createStatement();
+            ResultSet rs= smt.executeQuery(sql);
+            while (rs.next()){
+                System.out.println(rs.getInt(1)+"|"+rs.getString(2)+"|"+rs.getInt(3));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
